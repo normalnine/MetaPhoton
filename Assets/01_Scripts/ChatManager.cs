eb86b04a7889c893fb4d31762cbf69c526b468e8
normalnine <codejamgg@gmail.com>
2025-03-15 01:53:49 +0900

    Scroll View에 채팅 내용 추가

--- Assets/01_Scripts/ChatManager.cs
+++ Assets/01_Scripts/ChatManager.cs
@@ -8,6 +8,11 @@ public class ChatManager : MonoBehaviour
     // InputField
     public InputField chatInput;
 
+    // ChatItem Prefab
+    public GameObject chatItemFactory;
+    // ScrollView에 있는 Content의 RectTransform
+    public RectTransform rtContent;
+
     void Start()
     {
         // 엔터키를 누르면 InputField에 있는 텍스트 내용 알려주는 함수 등록
@@ -27,7 +32,15 @@ public class ChatManager : MonoBehaviour
 
     void OnSubmit(string s)
     {
-        print("OnSubmit : " + s);
+        //print("OnSubmit : " + s);
+        // ChatItem 을 만든다.
+        GameObject chatItem = Instantiate(chatItemFactory);
+        // 만들어진 item의 부모를 content로 한다.
+        chatItem.transform.SetParent(rtContent);
+        // 만들어진 item에서 Text 컴포넌트를 가져온다.
+        Text item = chatItem.GetComponent<Text>();
+        // 가져온 컴포넌트에서 text 값을 s 로 셋팅
+        item.text = s;
     }
 
     void OnValueChanged(string s)
