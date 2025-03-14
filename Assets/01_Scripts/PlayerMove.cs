a1e611ba4d6370088a1f1bb00cbd97d0de94f6f8
normalnine <codejamgg@gmail.com>
2025-03-14 16:49:34 +0900

    내 Player만 카메라 회전, 이동

--- Assets/01_Scripts/PlayerMove.cs
+++ Assets/01_Scripts/PlayerMove.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class PlayerMove : MonoBehaviour
+public class PlayerMove : MonoBehaviourPun
 {
     // 속력
     float speed = 5;
@@ -17,52 +18,56 @@ public class PlayerMove : MonoBehaviour
     // y 속력
     float yVelocity = 0;
 
-    // Start is called before the first frame update
     void Start()
     {
+        // Character Controller 가져오자
         cc = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // W, S, A, D 키를 누르면 앞뒤좌우로 움직이고 싶다.
-
-        // 1. 사용자의 입력을 받자.
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-
-        // 2. 방향을 만든다
-        // 좌우
-        Vector3 dirH = transform.right * h;
-        // 앞뒤
-        Vector3 dirV = transform.forward * v;
-        // 최종
-        Vector3 dir = dirH + dirV;
-        dir.Normalize();
-
-        // 만약에 땅에 닿아있다면
-        if(cc.isGrounded == true)
+        // 내가 만든 플레어이라면
+        if (photonView.IsMine)
         {
-            // yVelocity 를 0 으로 하자
-            yVelocity = 0;
+            // W, S, A, D 키를 누르면 앞뒤좌우로 움직이고 싶다.
+
+            // 1. 사용자의 입력을 받자.
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+
+            // 2. 방향을 만든다
+            // 좌우
+            Vector3 dirH = transform.right * h;
+            // 앞뒤
+            Vector3 dirV = transform.forward * v;
+            // 최종
+            Vector3 dir = dirH + dirV;
+            dir.Normalize();
+
+            // 만약에 땅에 닿아있다면
+            if(cc.isGrounded == true)
+            {
+                // yVelocity 를 0 으로 하자
+                yVelocity = 0;
+            }
+
+            // 스페이스바를 누르면 점프를 하고 싶다.
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                // yVelocity 에 jumpPower 를 셋팅
+                yVelocity = jumpPower;
+            }
+
+            // yVelocity 를 중력만큼 감소시키자.
+            yVelocity += gravity * Time.deltaTime;
+
+            // yVelocity 값을 dir 의 y 값에 셋팅
+            dir.y = yVelocity;
+
+            // 3. 그 방향으로 움직이자.
+            // transform.position += dir * speed * Time.deltaTime;
+            cc.Move(dir * speed * Time.deltaTime);
+
         }
-
-        // 스페이스바를 누르면 점프를 하고 싶다.
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            // yVelocity 에 jumpPower 를 셋팅
-            yVelocity = jumpPower;
-        }
-
-        // yVelocity 를 중력만큼 감소시키자.
-        yVelocity += gravity * Time.deltaTime;
-
-        // yVelocity 값을 dir 의 y 값에 셋팅
-        dir.y = yVelocity;
-
-        // 3. 그 방향으로 움직이자.
-        // transform.position += dir * speed * Time.deltaTime;
-        cc.Move(dir * speed * Time.deltaTime);
     }
 }
--- Assets/01_Scripts/PlayerRot.cs
+++ Assets/01_Scripts/PlayerRot.cs
@@ -1,40 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class PlayerRot : MonoBehaviour
+public class PlayerRot : MonoBehaviourPun
 {
-    //누적된 회전 값
+    // 누적된 회전 값
     float rotX;
     float rotY;
 
-    //회전 속력
+    // 회전 속력
     float rotSpeed = 200;
 
     // 카메라 Transform
     public Transform trCam;
 
-    // Start is called before the first frame update
     void Start()
     {
-
+        // 내가 생성한 Player 일 때만 카메라를 활성화 하자
+        if(photonView.IsMine)
+        {
+            trCam.gameObject.SetActive(true);
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //마우스의 움직임따라 플레이를 좌우 회전하고
-        //카메라를 위아카래 회전하고 싶다.
+        // 내것이 아닐 때 함수를 나가자
+        if (photonView.IsMine == false) return;
 
-        //1. 마우스 입력을 받자.
+        // 마우스의 움직임따라 플레이를 좌우 회전하고
+        // 카메라를 위아카래 회전하고 싶다.
+
+        // 1. 마우스 입력을 받자.
         float mx = Input.GetAxis("Mouse X");
         float my = Input.GetAxis("Mouse Y");
 
-        //2. 마우스의 움직임 값을 누적
+        // 2. 마우스의 움직임 값을 누적
         rotX += mx * rotSpeed * Time.deltaTime;
         rotY += my * rotSpeed * Time.deltaTime;
 
-        //3. 누적된 값만큼 회전 시키자.
+        // 3. 누적된 값만큼 회전 시키자.
         transform.localEulerAngles = new Vector3(0, rotX, 0);
         trCam.localEulerAngles = new Vector3(-rotY, 0, 0);
     }
