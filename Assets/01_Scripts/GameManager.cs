d1b21c9477f89cb24fbcc5e6aeaa467a7e8422ae
normalnine <codejamgg@gmail.com>
2025-03-14 18:25:36 +0900

    이동 회전 동기화 PhotonTransfromView

--- Assets/01_Scripts/GameManager.cs
+++ Assets/01_Scripts/GameManager.cs
@@ -7,6 +7,9 @@ public class GameManager : MonoBehaviour
 {
     void Start()
     {
+        // OnPhotonSerializeView 호출 빈도
+        PhotonNetwork.SerializationRate = 60;
+
         // 나의 Player 생성
         PhotonNetwork.Instantiate("Player", Vector3.zero, Quaternion.identity);
     }
