8bcb6aac1b67d14fc55002f426be0f807b7ad85b
normalnine <codejamgg@gmail.com>
2025-03-18 18:05:02 +0900

    ConnectionScene 만들기

---
+++ Assets/01_Scripts/ConnectionManager.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class ConnectionManager : MonoBehaviourPunCallbacks
+{
+    // NickName InputField
+    public InputField inputNickName;
+
+    // Connect Button
+    public Button btnConnect;
+
+    void Start()
+    {
+        // inputNickName의 내용이 변경될 때 호출되는 함수 등록
+        inputNickName.onValueChanged.AddListener(OnValueChanged);
+
+        // inputNickName에서 엔터 쳤을 때 호출되는 함수 등록
+        inputNickName.onSubmit.AddListener(
+            (string s) => {
+                // 버튼이 활성화 되어있다면
+                if(btnConnect.interactable)
+                {
+                    // OnClickConnect 호출
+                    OnClickConnect();
+                }
+            }
+        );
+
+        // 버튼 비활성화
+        btnConnect.interactable = false;
+    }
+
+    void OnValueChanged(string s)
+    {
+        btnConnect.interactable = s.Length > 0;
+
+        //// 만약에 s의 길이가 0보다 크면
+        //if(s.Length > 0)
+        //{
+        //    // 접속 버튼을 활성화
+        //    btnConnect.interactable = true;
+        //}
+        //// 그렇지 않으면 (s의 길이가 0)
+        //else
+        //{
+        //    // 접속 버튼을 비활성화
+        //    btnConnect.interactable = false;
+        //}
+    }
+
+    public void OnClickConnect()
+    {
+        // 서버 접속 요청
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+
+        // 닉네임 설정
+        PhotonNetwork.NickName = inputNickName.text;
+
+        // 로비 진입 요청
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+
+        print(System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+        // 로비 씬으로 이동
+    }
+}
